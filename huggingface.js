efddd2a955d20c24aaa26db446015a38d1387bf4
S. M. V <117322465+SMVthe1st@users.noreply.github.com>
2024-01-22 19:34:42 -0500

    Update huggingface.js

--- huggingface.js
+++ huggingface.js
@@ -2,7 +2,7 @@ import { textGenerationStream } from "@huggingface/inference";
 
 for await (
   const output of textGenerationStream ({
-    accessToken: "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",
+    accessToken: "HF_ACCESS_TOKEN",
     model: "HuggingFaceH4/zephyr-7b-beta",
     inputs: "Hello there!",
     parameters: { max_length: 100 }
